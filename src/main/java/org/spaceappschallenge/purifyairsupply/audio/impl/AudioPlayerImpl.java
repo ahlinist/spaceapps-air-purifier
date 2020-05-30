adaa133dcbb27b082fb1ceddf2803bbdc0148d45
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-05-30 17:05:12 +0300

    Drop inputStream

--- src/main/java/org/spaceappschallenge/purifyairsupply/audio/impl/AudioPlayerImpl.java
+++ src/main/java/org/spaceappschallenge/purifyairsupply/audio/impl/AudioPlayerImpl.java
@@ -2,12 +2,12 @@ package org.spaceappschallenge.purifyairsupply.audio.impl;
 
 import lombok.SneakyThrows;
 import org.spaceappschallenge.purifyairsupply.audio.AudioPlayer;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import java.io.File;
 import java.net.URL;
 
 @Component
@@ -16,11 +16,12 @@ public class AudioPlayerImpl implements AudioPlayer {
     @Override
     @SneakyThrows
     public void play(String path) {
-        URL url = new File(path).toURI().toURL();
-        AudioInputStream audioInputStream =  AudioSystem.getAudioInputStream(url);
-        Clip clip = AudioSystem.getClip();
-        clip.open(audioInputStream);
-        clip.start();
-        Thread.sleep(5000);
+        URL url = new ClassPathResource("audio/alarm.wav").getURL();
+
+        try(AudioInputStream audioInputStream =  AudioSystem.getAudioInputStream(url); Clip clip = AudioSystem.getClip()) {
+            clip.open(audioInputStream);
+            clip.start();
+            Thread.sleep(5000);
+        }
     }
 }
