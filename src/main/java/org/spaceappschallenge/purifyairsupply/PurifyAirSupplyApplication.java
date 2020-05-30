195c7679eef7ca643c1c94a8512668b82128c01c
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-05-30 11:21:00 +0300

    Init spring

---
+++ src/main/java/org/spaceappschallenge/purifyairsupply/PurifyAirSupplyApplication.java
@@ -0,0 +1,13 @@
+package org.spaceappschallenge.purifyairsupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurifyAirSupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurifyAirSupplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/spaceappschallenge/purifyairsupply/PurifyAirSupplyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spaceappschallenge.purifyairsupply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurifyAirSupplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
