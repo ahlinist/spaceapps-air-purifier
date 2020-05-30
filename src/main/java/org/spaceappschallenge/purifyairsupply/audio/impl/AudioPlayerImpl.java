53e74cef6026f7aafd9b7437fd00760cb5731b31
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-05-30 16:33:06 +0300

    Drop inputStream

--- src/main/java/org/spaceappschallenge/purifyairsupply/audio/impl/AudioPlayerImpl.java
+++ src/main/java/org/spaceappschallenge/purifyairsupply/audio/impl/AudioPlayerImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import java.io.File;
 import java.net.URL;
 
 @Component
@@ -16,6 +17,7 @@ public class AudioPlayerImpl implements AudioPlayer {
     @SneakyThrows
     public void play(String path) {
         URL url = AudioPlayerImpl.class.getResource(path);
+        //URL url =new File(path).toURI().toURL();
         AudioInputStream audioInputStream =  AudioSystem.getAudioInputStream(url);
         Clip clip = AudioSystem.getClip();
         clip.open(audioInputStream);
