db6cd49a020be2f68b05813d04ca054d56b9b44b
Anton Hlinisty <anton.hlinisty@gmail.com>
2020-05-31 12:17:49 +0300

    Playback sound for 3 secs

--- src/main/java/org/spaceappschallenge/purifyairsupply/audio/impl/AudioPlayerImpl.java
+++ src/main/java/org/spaceappschallenge/purifyairsupply/audio/impl/AudioPlayerImpl.java
@@ -21,7 +21,7 @@ public class AudioPlayerImpl implements AudioPlayer {
         try(AudioInputStream audioInputStream =  AudioSystem.getAudioInputStream(url); Clip clip = AudioSystem.getClip()) {
             clip.open(audioInputStream);
             clip.start();
-            Thread.sleep(2000);
+            Thread.sleep(3000);
         }
     }
 }
